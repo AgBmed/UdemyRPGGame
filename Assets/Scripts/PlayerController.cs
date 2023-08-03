e726b123b8b1b9f479cac4de603fd2a6a0a3766e
Agustin Gutierrez <agustin.bmed@gmail.com>
2023-08-02 22:41:13 -0400

    Udemy RPG Game: game assets and configurations
    All files needed to begin with my course 2D Unity game!
    Including LFS hopefully -> seems ok.
    Removing log files
    
    Signed-off-by: Agustin Gutierrez <agustin.bmed@gmail.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Rigidbody2D theRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = 1;
+    }
+}
