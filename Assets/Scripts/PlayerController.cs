659dbd6d377955eab8d317b04379ab1d1f3c7a3f
Agustin Gutierrez <agustin.bmed@gmail.com>
2023-08-08 00:35:29 -0400

    Udemy RPG Game - Animation Hooks engaged
    Signed-off-by: Agustin Gutierrez <agustin.bmed@gmail.com>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     public Rigidbody2D theRB;
     public float moveSpeed;
+    public Animator myAnim;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
     }
 }
